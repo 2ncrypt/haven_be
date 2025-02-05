f27375a22b168e3c025b7b1227345b5c88fdfa33
HyunSu Kim <me_2_day_@naver.com>
2025-02-05 20:45:25 +0900

    Haven first commit

---
+++ src/main/java/com/pain/haven/HavenApplication.java
@@ -0,0 +1,13 @@
+package com.pain.haven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pain/haven/HavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pain.haven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
